3c22d081b4a24055394bf26cc2a4479c3fb153d6
Baran Algül <baran.a12@gso.schule.koeln>
2022-09-01 10:55:32 +0200

    Aufgaben bearbeitet

--- Geist/Program.cs
+++ Geist/Program.cs
@@ -4,3 +4,21 @@ Console.WriteLine("(o o)");
 Console.WriteLine("| O |");
 Console.WriteLine("|   |");
 Console.WriteLine("'~~~'");
+
+string Name = "Baran";
+string Nachname = "Algül";
+string Alter = "17";
+Console.WriteLine($"{Name} {Nachname} Ist {Alter} Jahre alt");
+
+string Vorname;
+string Alter1;
+string Klasse;
+
+Console.WriteLine("name?");
+Vorname = Console.ReadLine();
+Console.WriteLine("Alter?");
+Alter1 = Console.ReadLine();
+Console.WriteLine("Klasse?");
+Klasse = Console.ReadLine();
+
+Console.WriteLine($"{Vorname} ist {Alter1} Jahre alt und besucht die {Klasse} des Georg-Simon-Ohm Berufskollegs.");
