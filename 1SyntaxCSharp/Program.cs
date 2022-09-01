868968fdec842024868ca642101e8ac516485011
Baran Algül <baran.a12@gso.schule.koeln>
2022-09-01 10:33:05 +0200

    geist

--- 1SyntaxCSharp/Program.cs
+++ 1SyntaxCSharp/Program.cs
@@ -1,7 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("teyze");
-//TEST
-Console.WriteLine("kalkmama yardim et");
-Console.WriteLine("ne");
-Console.WriteLine("teyzesi");
-Console.WriteLine("dayim");
+Console.WriteLine();
+
+
---
+++ Geist/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" .-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
